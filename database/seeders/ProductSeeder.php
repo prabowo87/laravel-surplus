39592f26883e7b75dc87db44eae0dec833b71874
prabowo87 <mboh.94er0h@gmail.com>
2023-02-12 08:50:56 +0700

    added seedder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use DB;
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+ 
+    	for($i = 1; $i <= 20; $i++){
+    		DB::table('product')->insert([
+    			'name' => 'product '.$i,
+    			'description' => $faker->text(100),
+    			'enable' => $faker->boolean(),
+    		]);
+ 
+    	}
+    }
+}
